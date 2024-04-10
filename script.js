9a4194696cd1840405f7c49549b5ca68a1e13173
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-04-10 20:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const userNameHTML = document.getElementById("username");
+const passwordHTML = document.getElementById("password");
+const checkHTML = document.getElementById("checkbox");
+const existingHTML = document.getElementById("existing");
+const handleSubmit = () => {
+	if(checkHTML.checked){
+		localStorage.set
+	}
+}
